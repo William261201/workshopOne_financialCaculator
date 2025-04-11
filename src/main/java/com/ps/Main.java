68505207eb50d72a1df22389d1898fbd9fb2b565
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-04-11 13:43:00 -0400

    project created

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,22 @@
+package com.ps;
+import java.util.Scanner
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Mortgage Calculator");
+
+        Scanner scanner = new Scanner(System.in);
+
+        // get user's loan input with a variable 'loan_amount'
+        System.out.print("What is the loan amount?: ");
+        int loan_amount = scanner.nextInt();
+
+        // get user's monthly interest rate with a variable 'interest_rate'
+        System.out.print("What is the monthly interest rate?: ");
+        double percentage = scanner.nextFloat();
+        double interest_rate = ()
+
+    }
+}
