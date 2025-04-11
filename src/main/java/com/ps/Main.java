488737ca45da24c75bdacdcb3bd06690da43c29d
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-04-11 14:34:49 -0400

    Mortgage Calculator Project Done

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,5 +1,5 @@
 package com.ps;
-import java.util.Scanner
+import java.util.Scanner;
 
 import java.util.Scanner;
 
@@ -9,14 +9,38 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        // get user's loan input with a variable 'loan_amount'
+        ///get user's loan amount input with a variable 'P'
         System.out.print("What is the loan amount?: ");
-        int loan_amount = scanner.nextInt();
+        int P = scanner.nextInt();
+        //System.out.print(P);
 
-        // get user's monthly interest rate with a variable 'interest_rate'
-        System.out.print("What is the monthly interest rate?: ");
-        double percentage = scanner.nextFloat();
-        double interest_rate = ()
+        ///get how many months the payment would be
+        System.out.print("How many years are you paying this amount?: ");
+        int payment_years = scanner.nextInt();
+        //change payment years to months and store the value with 'n'
+        int n = payment_years * 12;
+        //System.out.print(n);
+
+        /// get user's monthly interest rate with a variable 'interest_rate'
+        ///calculate annual rate first then divide by 12 to get monthly interest rate
+        System.out.print("What is the annual interest rate?: ");
+        double annual_interest_percentage = scanner.nextFloat();
+        double r = annual_interest_percentage/(12*100);
+        //System.out.print(r);
+
+        ///calculate the monthly payment
+        double M;
+        double interest_rate_power = Math.pow((1+r), n);
+        M = (P * r * interest_rate_power)/(interest_rate_power - 1);
+
+        //print out monthly payment amount for user's loan
+        System.out.printf("Your monthly payment for loan %d is %.2f\n", P, M );
+
+        ///calculate total payment and print out the amount
+        double Total_paid = M * n;
+        System.out.printf("The total payment after %d year is %.2f", payment_years, Total_paid);
+
+
 
     }
 }
